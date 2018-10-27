2a96445c4a68d0357bc01398acf5d963a047bed6
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2018-10-27 17:43:59 +0300

    Sceleton for proc nested_text_fold

--- TCL/nested_text.tcl
+++ TCL/nested_text.tcl
@@ -5,6 +5,34 @@ set ::_CLOSE 1 ;   # means closing bracket
 
 set ::_nt_stack [list] ;  # for FIFO list of {type str} records (lists)
 
+
+# Returns folded version of 'inpTxt' folded around 'beginTag' and  'endTag'.
+# 'beginTag' and  'endTag' are regular-expression patterns.
+# 'numLevels' tells how many nesting levels to fold - from innermost. 0 means all.
+# On error returns 0.
+proc nested_text_fold {inpTxt beginTag endTag numLevels}  {
+  set tail $inpTxt ;  # unprocessed yet text
+  set nestList [nested_text_to_list $inpTxt $beginTag $endTag]
+  if { $nestList == 0 }  {  return  0 };  # error already printed
+  set outTxt [nested_text_from_list $nestList $beginTag $endTag $numLevels]
+  if { $outTxt == 0 }  {  return  0 };  # error already printed
+  return  $outTxt
+}
+
+
+# 
+proc nested_text_to_list {inpTxt beginTag endTag} {
+  # TODO: implement
+}
+
+
+# 
+proc nested_text_from_list {nestList beginTag endTag numLevels}   {
+  # TODO: implement
+}
+
+
+############################ Local utilites ####################################
 # Puts 'str' onto stack as type 'openOrClose' ($::_OPEN / $::_CLOSE)
 proc _nested_text_push {openOrClose str} {
   lappend ::_nt_stack [list $openOrClose $str]
@@ -58,3 +86,4 @@ proc _nested_text_top_is_closer {} {
   if { 0 == [_nested_text_top openOrClose_ str_] }  { return  0 }; # empty or error
   return  [expr {$openOrClose_ == $::_CLOSE}]
 }
+############################ End of local utilites #############################
